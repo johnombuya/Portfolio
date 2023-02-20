d4906094204da7b0ab73fadb9882e84c18a8e520
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-02-20 10:18:31 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 from flask_bootstrap import Bootstrap
 
 from portfolioManager import *
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config["SECRET_KEY"] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
 Bootstrap(app)
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return render_template('index.html', projects=Project.select())
+    return render_template("index.html", projects=Project.select())
 
-@app.route('/about')
+
+@app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template("about.html")
 
-@app.route('/contact')
+
+@app.route("/contact")
 def contact():
-    return render_template('contact.html')
+    return render_template("contact.html")
 
-if __name__ == '__main__':
+
+@app.route("/download")
+def download_file():
+    # specify the path to your file here
+    path = "static/docs/johnombuya-resume.pdf"
+    # set the name you want the file to be downloaded as
+    filename = "John Ombuya Resume.pdf"
+    # return the file as a downloadable attachment
+    return send_file(path, as_attachment=True, attachment_filename=filename)
+
+
+if __name__ == "__main__":
     app.run(debug=True)
